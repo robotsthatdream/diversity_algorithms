41d6fee7fa334e613bdc27538439b5f077cbd520
Alexandre Coninx <alexandre.coninx@isir.upmc.fr>
2019-08-02 13:18:06 +0200

    Remove debug output

--- diversity_algorithms/controllers/deap_tools_dnn.py
+++ diversity_algorithms/controllers/deap_tools_dnn.py
@@ -28,7 +28,9 @@ def mutDNN(dnn, # Individual to mutte
 		n_max_hidden=30, # Maximum number of hidden neurons
 		n_min_conns=8, # Minimum number of edges
 		n_max_conns=250 # Maximum number of edges
-		): 
+		):
+	#print("Before mutation:")
+	#dnn.describe()
 	# Mutate weights
 	for e in dnn.nn.edges():
 		w = dnn.nn.ep.weights[e]
@@ -42,21 +44,23 @@ def mutDNN(dnn, # Individual to mutte
 		dnn.nn.vp.bias[v] = new_b
 	# Add connection
 	if((dnn.n_conns() < n_max_conns) and (np.random.random() < mutation_rate_add_conn)):
-		print("Added conn!!")
+		#print("Added conn!!")
 		dnn.add_random_conn()
 	# Del connection
 	if((dnn.n_conns() > n_min_conns) and (np.random.random() < mutation_rate_del_conn)):
-		print("Deleted conn!!")
+		#print("Deleted conn!!")
 		dnn.del_random_conn()
 	# Add node on a random edge
 	if((dnn.n_hidden() < n_max_hidden) and (np.random.random() < mutation_rate_add_node)):
-		print("Added node!!")
+		#print("Added node!!")
 		dnn.add_node_on_random_edge()
 	
 	# Del random hidden node
 	if((dnn.n_hidden() > n_min_hidden) and (np.random.random() < mutation_rate_del_node)):
-		print("Deleted node!!")
+		#print("Deleted node!!")
 		dnn.del_random_hidden()
+	#print("After mutation:")
+	#dnn.describe()
 	return (dnn,)
 
 
