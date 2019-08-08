f62125c02fe197642f610f6bb765261c18c16802
Alexandre Coninx <alexandre.coninx@isir.upmc.fr>
2019-08-08 13:05:49 +0200

    Update when/if evolvability is computed

--- diversity_algorithms/experiments/maze_ns_exploration_dnn.py
+++ diversity_algorithms/experiments/maze_ns_exploration_dnn.py
@@ -79,9 +79,9 @@ def launch_nov(pop_size, nb_gen, evolvability_period=0, dump_period_pop=10, dump
 	stats=None
 	nbc=nb_bin**2
 	nbs=nbc*2 # min 2 samples per bin
-	evolvability_nb_samples=0# disable that for now nbs
+	evolvability_nb_samples=nbs
 	window_global=nbs/pop_size
-	if (evolvability_nb_samples>0):
+	if (evolvability_period>0) and (evolvability_nb_samples>0):
 		stats=get_stat_coverage(grid,indiv=True,min_x=min_x,max_x=max_x,nb_bin=nb_bin, gen_window_global=window_global)
 	else:
 		stats=get_stat_coverage(grid,indiv=False,min_x=min_x,max_x=max_x,nb_bin=nb_bin, gen_window_global=window_global)
@@ -121,7 +121,7 @@ def launch_nov(pop_size, nb_gen, evolvability_period=0, dump_period_pop=10, dump
 	}
 	
 	print("Launching Novelty Search with pop_size=%d, nb_gen=%d and evolvability_nb_samples=%d"%(pop_size, nb_gen, evolvability_nb_samples))
-	if (evolvability_nb_samples>0):
+	if (evolvability_period>0) and (evolvability_nb_samples>0):
                 print("WARNING, evolvability_nb_samples>0. The run will last much longer...")
 
 	if with_scoop:
