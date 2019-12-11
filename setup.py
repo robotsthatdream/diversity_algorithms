b75d486e2b3e3eba39ed7ecc5edc9a5b376d29b8
Alexandre Coninx <alexandre.coninx@isir.upmc.fr>
2019-12-11 14:28:28 +0100

    Add missing setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='diversity_algorithms',
+      version='0.0.1',
+      install_requires=['gym>=0.2.3'],
+      packages=find_packages(),
+      author='Alex Coninx',
+      author_email='coninx@isir.upmc.fr'
+)
