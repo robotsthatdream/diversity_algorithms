c4bede10009756b37bc682217af662655aedbf52
Alex Coninx <alexandre.coninx@isir.upmc.fr>
2020-01-13 12:54:39 +0100

    Up to date dependancies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='diversity_algorithms',
       version='0.0.1',
-      install_requires=['gym>=0.2.3'],
+      install_requires=['gym>=0.2.3','deap','scoop','dill'],
       packages=find_packages(),
       author='Alex Coninx',
       author_email='coninx@isir.upmc.fr'
