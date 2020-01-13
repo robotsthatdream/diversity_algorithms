d08e0661abfcfb688c23cbda4df5cd5d48e5864f
Alex Coninx <alexandre.coninx@isir.upmc.fr>
2020-01-13 15:51:07 +0100

    MESU scripts

---
+++ mesu/launch_diversity_mesu.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#PBS -q alpha
+#PBS -l select=1:ncpus=64
+#PBS -N diversity_algo
+
+# Alex Coninx
+# ISIR - Sorbonne Universite / CNRS
+# 13/01/2020
+
+# Complete this to where the installation script was run
+DIVERSITY_BASE_DIR=/home/$USER/src/diversity
+RESULTS_DIR=/scratchalpha/$USER/diversity
+
+# Experiment specifications
+EXPERIMENT_SCRIPT="gym_novelty.py"
+DIVERSITY_ARGUMENTS=""
+
+# /!\ If you change this you should also probably manually change /!\
+#        the ncpus= directive in the PBS directive above
+N_WORKERS_SCOOP=64
+
+
+
+mkdir -p ${RESULTS_DIR}
+
+
+module purge
+module load python-3.6
+
+CMD_LINE="python -m scoop -n ${N_WORKERS_SCOOP} ${DIVERSITY_BASE_DIR}/diversity_algorithms_dev/diversity_algorithms/experiments/${EXPERIMENT_SCRIPT} ${DIVERSITY_ARGUMENTS}"
+
+cd $DIVERSITY_BASE_DIR
+
+source env/bin/activate
+
+cd $RESULTS_DIR
+
+${CMD_LINE}
