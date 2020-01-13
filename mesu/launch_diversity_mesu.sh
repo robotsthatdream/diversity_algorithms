038c3b1b45a24166ceaa71d2aa1c4507b116dda7
Alex Coninx <alexandre.coninx@sorbonne-universite.fr>
2020-01-14 00:05:15 +0100

    Fix script + add missing dep

--- mesu/launch_diversity_mesu.sh
+++ mesu/launch_diversity_mesu.sh
@@ -7,10 +7,14 @@
 # ISIR - Sorbonne Universite / CNRS
 # 13/01/2020
 
-# Complete this to where the installation script was run
-DIVERSITY_BASE_DIR=/home/$USER/src/diversity
-RESULTS_DIR=/scratchalpha/$USER/diversity
 
+USER_NAME=coninx
+
+# Complete this to where the installation script was run
+DIVERSITY_BASE_DIR=/home/${USER_NAME}/src/diversity
+RESULTS_DIR=/scratchalpha/${USER_NAME}/diversity
+
+# Experiment specifications
 # Experiment specifications
 EXPERIMENT_SCRIPT="gym_novelty.py"
 DIVERSITY_ARGUMENTS=""
@@ -20,6 +24,7 @@ DIVERSITY_ARGUMENTS=""
 N_WORKERS_SCOOP=64
 
 
+echo "Creating dir ${RESULTS_DIR} if not existing"
 
 mkdir -p ${RESULTS_DIR}
 
@@ -29,10 +34,21 @@ module load python-3.6
 
 CMD_LINE="python -m scoop -n ${N_WORKERS_SCOOP} ${DIVERSITY_BASE_DIR}/diversity_algorithms_dev/diversity_algorithms/experiments/${EXPERIMENT_SCRIPT} ${DIVERSITY_ARGUMENTS}"
 
+echo "Loading virtualenv env in $DIVERSITY_BASE_DIR"
+
 cd $DIVERSITY_BASE_DIR
 
 source env/bin/activate
 
+
+echo "Going to $RESULTS_DIR"
+
 cd $RESULTS_DIR
 
+echo "Command line will be : '$CMD_LINE'"
+
+echo "Running..."
+
 ${CMD_LINE}
+
+echo "Python terminated"
