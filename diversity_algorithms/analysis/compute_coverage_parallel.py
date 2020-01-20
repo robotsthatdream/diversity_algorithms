2173ae62c14550bd1c44b07d5f0308aee61968bc
Alex Coninx <alexandre.coninx@isir.upmc.fr>
2020-01-20 18:38:44 +0100

    Coverage computation

---
+++ diversity_algorithms/analysis/compute_coverage_parallel.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+""" Alex Coninx
+    ISIR - Sorbonne Universite / CNRS
+    20/01/2020
+""" 
+
+import numpy as np
+import sys, os
+
+from diversity_algorithms.experiments.exp_utils import RunParam, analyze_params
+import diversity_algorithms.analysis.data_utils as du
+from diversity_algorithms.analysis.population_analysis import get_coverage
+
+from scoop import futures
+
+params_commandline = {
+    "rundir": RunParam("d", "", "Directory of the run"),
+    "gen": RunParam("g", 0, "Generation to look up to"),
+    "evofile_pattern" : RunParam("", "evolvability_ind%d_bd_gen%d.npz", "Evolvabilitty data files pattern"),
+    "n_indivs": RunParam("", -1, "Number of indivs for which evolvability was generated (default = pop size)"),
+    "outfile_pattern": RunParam("","coverages_evolvability_gen%d.npz", "Output file pattern"),
+    "verbosity": RunParam("v",0, "Verbosity level")
+}
+
+analyze_params(params_commandline, sys.argv)
+
+
+gen = params_commandline["gen"].get_value()
+rundir = params_commandline["rundir"].get_value()
+evofile_pattern = params_commandline["evofile_pattern"].get_value()
+n_indivs = params_commandline["n_indivs"].get_value()
+outfile_pattern = params_commandline["outfile_pattern"].get_value()
+verbose=bool(params_commandline["verbosity"].get_value())
+
+if(gen==0 or rundir==""):
+	print("Please specify generation and rundir")
+	sys.exit(1)
+
+print("Reading environment parameters")
+
+try:
+	params_xp = dict(np.load(os.path.join(rundir, "params.npz"), allow_pickle=True))
+except FileNotFoundError:
+	print("Can't find param file -_-")
+	sys.exit(1)
+
+nb_bin=int(params_xp["nb_bin"])
+min_x=params_xp["min_bd"]
+max_x=params_xp["max_bd"]
+env_name=str(params_xp["env_name"])
+
+if(n_indivs < 0):
+	n_indivs = int(params_xp["pop_size"])
+
+
+print("Checking evolvability files")
+evofiles = list()
+
+for i in range(n_indivs):
+	filepath = os.path.join(rundir,evofile_pattern % (i, gen))
+	if(os.path.isfile(filepath)):
+		evofiles.append(filepath)
+	else:
+		print("WARNING: could not find '%s'" % filepath)
+
+def coverage_from_file(file,  min_x, max_x, nb_bin, verbose=False):
+	if(verbose):
+		print("Computing coverage for '%s'..." % file, end='')
+	points=du.get_points_from_genfile(file)
+	cov = get_coverage(min_x,max_x,nb_bin,points)
+	if (verbose):
+		print("Coverage: "+str(cov))
+	else:
+		print(".", end='', flush=True)
+	return cov
+
+
+if(__name__=='__main__'):
+	print("%d evofiles to process" % len(evofiles))
+	coverages = list(futures.map(lambda f: coverage_from_file(f, min_x, max_x, nb_bin, verbose), evofiles))
+	print("Done ! Saving data...")
+	outfile = os.path.join(rundir,outfile_pattern % gen) 
+	np.savez(outfile, coverages=coverages)
+	print("Evolvability data saved to %s" % outfile)
