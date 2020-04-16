c87fdaab9c27f47d6f93a2b43cc4c9aa673aba33
Alex Coninx <alex@coninx.org>
2020-04-16 18:32:10 +0200

    Version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='diversity_algorithms',
       install_requires=['gym>=0.2.3','deap','scoop','dill','alphashape'],
-      version='0.0.2',
+      version='0.1.0',
       packages=find_packages(),
       include_package_data=True,
       author='Alex Coninx',
