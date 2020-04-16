2b1c02c6f948b85cc6823331e6ffcffc4f7fff7d
Alex Coninx <alex@coninx.org>
2020-04-16 18:06:06 +0200

    Remove CMAES, CMANS, seedES and obsolete maze script

--- diversity_algorithms/algorithms/__init__.py
+++ diversity_algorithms/algorithms/__init__.py
@@ -4,4 +4,4 @@ from diversity_algorithms.algorithms.novelty_search import novelty_ea
 # No reason to expose other functions - they can be accessed through the submodule if needed
 
 
-__all__=["novelty_search", "stats", "utils"]
+__all__=["novelty_search", "stats", "utils", "novelty_management", "quality_diversity", "evolutionary_algorithms"]
