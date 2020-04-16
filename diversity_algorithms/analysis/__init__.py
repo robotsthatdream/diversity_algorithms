31fb27a223dc6ac596401251f02118bd2d87344f
Alex Coninx <alex@coninx.org>
2020-04-16 18:28:08 +0200

    Cleaned up MESU-related stuff and old analysis scripts

--- diversity_algorithms/analysis/__init__.py
+++ diversity_algorithms/analysis/__init__.py
@@ -2,4 +2,4 @@
 
 from diversity_algorithms.analysis.population_analysis import build_grid, update_grid, coverage, radius, sample_from_pop
 
-__all__ = ["population_analysis"]
+__all__ = ["compute_coverages_uniformity_parallel", "data_utils", "evolvability_ns_new", "evolvability_qd_archive", "phylogenetic_analysis", "population_analysis", "qd_postprocess_archives", "violin_plot"]
